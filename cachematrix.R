09d73bd0853aecfcbc3d98e4a476451641c75099
Somprabh Jha <somprabhjha@gmail.com>
2017-06-23 04:48:49 +0530

    Final completed programming assignment 2 - cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will take an input matrix and give its inverse as output.
+## The inverse matrix will come from cache if it has been inversed once
+## otherwise it will inverse the input matrix.
 
-## Write a short comment describing this function
+## This function will take an input matrix and create a similar dimensioned
+## output matrix. It will also create a list with 4 functions to be used by
+## the cacheSolve function.
 
 makeCacheMatrix <- function(x = matrix()) {
+        newrow <- nrow(x)
+        newcol <- ncol(x)
+        m <- matrix(NA,nrow=newrow,ncol=newcol)
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function will determine if the inverse of the input matrix already
+## exists in cache. If it does then the cache inverse matrix is returned
+## otherwise an inverse of the input matrix is created and cached in memory.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!all(is.na(m))) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setsolve(m)
+        m
 }
